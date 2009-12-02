1e96e5bc26c3deba0046fbd0d2e5c198a3794241
Povel Karlsson <povel.y@gmail.com>
2009-12-02 14:56:57 +0200

    uninstall message change

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,6 @@
-puts "don't forget to rollback create_versions_for_* migrations and detele acts_as_versinable from your models\n"
+puts "
+*IMPORTANT*
+
+Don't forget to rollback create_versions_for_* migrations and detele acts_as_versinable from your models
+
+"
